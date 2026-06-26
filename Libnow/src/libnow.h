3b6df4e9133867b06fc24323f0fc31d089610882
blfuentes <epiphonetodie@gmail.com>
2026-06-26 18:31:06 +0200

    refactor: setting up workspace, added libnow

---
+++ Libnow/src/libnow.h
@@ -0,0 +1,21 @@
+#include <stdint.h>
+#ifndef __LIBNOW_H__
+#define __LIBNOW_H__
+
+
+typedef struct {
+    int left_control;
+    int right_control;
+} message_control_status;
+
+typedef enum{
+    DST_ROBOT = 0,
+    DST_MANDO
+} LibNowDst;
+
+void libnow_init();
+
+void libnow_addPeer(LibNowDst dst);
+void libnow_sendMessage(LibNowDst dst, message_control_status *msg);
+
+#endif
